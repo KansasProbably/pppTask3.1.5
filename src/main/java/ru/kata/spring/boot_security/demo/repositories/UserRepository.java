5bc5d0018a4febb726c9ed372d89410da145ef90
”Nick <lilmodou14@gmail.com>
2024-07-20 10:23:35 +0300

    @query

--- src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package ru.kata.spring.boot_security.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.models.User;
 
@@ -9,10 +11,7 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findUserByEmail(String email);
-    void addUser(User user);
-    List<User> findAll();
-    void delete(Long id);
-    void update(User user);
-    User getUserById(Long id);
+    @Query ("select u from User u join fetch u.roles where u.email = :email")
+    User findUserByEmail(@Param("email") String email);
+
 }
--- src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
-import ru.kata.spring.boot_security.demo.repositories.UserRepositoryImpl;
+
 
 
 import javax.persistence.EntityManager;
@@ -24,13 +24,13 @@ import java.util.stream.Collectors;
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserRepositoryImpl userRepository;
+    private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
 
 
     @Autowired
-    public UserServiceImpl(UserRepositoryImpl userRepository, @Lazy PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
@@ -42,29 +42,29 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(long id) {
-        return userRepository.getUserById(id);
+        return userRepository.findById(id).orElse(null);
     }
 
     @Override
     @Transactional
     public void saveUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.addUser(user);
+        userRepository.save(user);
 
     }
 
     @Override
     @Transactional
     public void updateUser(User user) {
-        if (!user.getPassword().equals(userRepository.getUserById(user.getId()).getPassword())) {
+        if (!user.getPassword().equals(userRepository.getById(user.getId()).getPassword())) {
             user.setPassword(passwordEncoder.encode(user.getPassword()));
         }
-        userRepository.addUser(user);
+        userRepository.save(user);
     }
 
     @Override
     public void deleteUser(long id) {
-        userRepository.delete(id);
+        userRepository.deleteById(id);
     }
 
 
